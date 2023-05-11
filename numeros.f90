42c0b6059cb479507e45b0b9186c5260ef74aef1
Ramiro <r.gg.3@outlook.com>
2023-05-11 04:48:09 -0600

    diferentes representaciones de pi en diferentes tipos de datos para los numeros

--- numeros.f90
+++ numeros.f90
@@ -1,10 +1,14 @@
 program numeros
 implicit none
-integer :: a 
-real :: b
-double precision :: c
+integer :: a , pi_1
+real :: b , pi_2
+double precision :: c , pi_3
 a = 1
 b = 1.0
 c = 1.0
+pi_1 = ACOS(-1.0)
+pi_2 = ACOS(-1.0)
+pi_3 = ACOS(-1.0)
 print * , a , b , c
+print * , pi_1 , pi_2 , pi_3
 end program
